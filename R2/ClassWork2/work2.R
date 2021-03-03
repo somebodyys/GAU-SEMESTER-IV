fd9898bc1ae25aa34b7e9fd69eaed48476b23952
somebodyys <tkokikaca1@gmail.com>
2021-03-03 15:33:12 +0400

    added interval plotting

--- R2/ClassWork2/work2.R
+++ R2/ClassWork2/work2.R
@@ -1,7 +1,6 @@
 #load
 myData <- read.csv("HouseholdData.csv")
 
-
 #a
 stem(myData$age)
 hist(myData$age, xlab = "Age")
@@ -14,16 +13,29 @@ for (i in 1:1000) {
 hist(result)
 ###########################################
 
-#experiment used qt
+#experiment 1 using qt
 confidence <- 95
 tail <- (1 - confidence/100)/2 + confidence /100
 result <- matrix(ncol = 2)
 size <- 9
-for (i in 1:1000) {
+iterations <- 100
+for (i in 1:iterations) {
   picked <- sample(myData$age, size)
   result <- rbind(result, mean(picked) + c(-1,1) * (qt(tail, size - 1)* sd(picked))/sqrt(size))
 }
+result <- result[-1,]
 
+#plotting
+plot(1, type="n", xlab="", ylab="", xlim=c(20, 80), ylim=c(1, iterations))
+segments(x0=result[,1], 
+         x1=result[,2], 
+         y0=1:iterations, 
+         y1=1:iterations,
+         col = ifelse(mean(myData$age) >= result[,1] & mean(myData$age) <= result[,2], "green", "red"))
+abline(v=mean(myData$age), col="blue", lty=5,lwd=2)
+legend("bottomright",
+       legend=c("Failed", "Population Mean", "Caught"),
+       col=c("red", "blue", "green"), lty=1:2, cex = 0.4)
 
 #check validity
 check <- c()
@@ -36,3 +48,5 @@ for (i in 4:length(result)/2) {
   check <- append(check, out)
 }
 length(check[check == 1])/length(check)*100
+
+
