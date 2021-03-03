f34b8ae79401afacea017886b2a1c9a3715b4ee3
somebodyys <tkokikaca1@gmail.com>
2021-03-03 15:59:09 +0400

    added sample mean points and connected with lines

--- R2/ClassWork2/work2.R
+++ R2/ClassWork2/work2.R
@@ -18,7 +18,7 @@ confidence <- 95
 tail <- (1 - confidence/100)/2 + confidence /100
 result <- matrix(ncol = 2)
 size <- 9
-iterations <- 100
+iterations <- 30
 for (i in 1:iterations) {
   picked <- sample(myData$age, size)
   result <- rbind(result, mean(picked) + c(-1,1) * (qt(tail, size - 1)* sd(picked))/sqrt(size))
@@ -33,9 +33,16 @@ segments(x0=result[,1],
          y1=1:iterations,
          col = ifelse(mean(myData$age) >= result[,1] & mean(myData$age) <= result[,2], "green", "red"))
 abline(v=mean(myData$age), col="blue", lty=5,lwd=2)
+points(x=(result[,1]+result[,2])/2, 
+       y=1:iterations,
+       cex=0.6,
+       col="purple")
+lines(x=(result[,1]+result[,2])/2, 
+      y=1:iterations,
+      col="purple")
 legend("bottomright",
-       legend=c("Failed", "Population Mean", "Caught"),
-       col=c("red", "blue", "green"), lty=1:2, cex = 0.4)
+       legend=c("Failed", "Population Mean", "Caught","Sample Mean"),
+       col=c("red", "blue", "green", "purple"), lty=c(1,2,1,1), cex = 0.4)
 
 #check validity
 check <- c()
