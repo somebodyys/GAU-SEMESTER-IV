0ead26b8f62da56d672a3a30e737be536e8a66d5
somebodyys <tkokikaca1@gmail.com>
2021-03-12 22:07:54 +0400

    Finished ClassWork1

---
+++ C#/1_ClassWork_Solution/1_ClassWork_Functions/Tasks.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork1_Functions
+{
+    public static class Tasks
+    {
+
+        //Check if building right_angle triangle could be built.
+        //Returns bool
+        public static bool task_2_1_31(float a, float b, float c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0) throw new Exception("Triangle sides must be positive!");
+            return (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a);
+        }
+
+        //Returns List of Elements That are multiples of 5
+        public static List<int> task_3_2_12(List<int> input)
+            => input.Where(i => i % 5 == 0).ToList();
+     
+
+        public static int task_3_2_12A(int x, int y)
+        {
+            if (x == y)
+                throw new Exception("These Numbers Are Equal!");
+            bool x_reminder = (x % 5 == 0);
+            bool y_reminder = (y % 5 == 0);
+            if (x_reminder ^ y_reminder)
+                throw new Exception("Both Are Dividable by 5");
+            return (x_reminder ? x : y);
+        }
+
+
+        //Returns Fibonachi numbers by Index
+        public static int task_3_4_10(int index)
+        {
+            if (index < 0)
+                throw new Exception("Only positive index!");
+            if (index == 0) return 0;
+            if (index == 1) return 1;
+            return task_3_4_10(index - 1) + task_3_4_10(index - 2);
+        }
+
+
+        //Replace negative Numbers by 0
+        public static int[] task_4_1_43(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++) numbers[i] = numbers[i] < 0 ? 0 : numbers[i];
+            return numbers;
+        }
+
+
+        //Returns List of even numbers from 2D array
+        public static int[] task_4_2_20(int[,] input)
+        {
+            List<int> result = new List<int>();
+            for (int row = 0; row < input.GetLength(0); row++)
+                for (int col = 0; col < input.GetLength(1); col++)
+                    if (input[row, col] % 2 == 0)
+                        result.Add(input[row, col]);
+            return result.ToArray();
+        }
+
+
+        //Returns the sum of items in top not main diagonal
+        public static int task_4_2_81(int[,] input)
+        {
+            if (input.GetLength(0) != input.GetLength(1))
+                throw new Exception("Only Quadratic Matrix accepted!");
+            int result = 0;
+            for (int row = 0; row < input.GetLength(0); row++)
+                for (int col = 0; col < input.GetLength(1) - row; col++)
+                    result += input[row, col];
+            return result;
+        }
+    }
+}
---
+++ C#/1_ClassWork_Solution/ClassWork1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassWork1_Functions;
+
+namespace ClassWork1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+
+                //2.1.31
+                float a, b, c;
+                Console.Write($"Enter a : ");
+                a = Convert.ToInt64(Console.ReadLine());
+                Console.Write($"Enter b : ");
+                b = Convert.ToInt64(Console.ReadLine());
+                Console.Write($"Enter c : ");
+                c = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine($"Task 2.1.31 Result : {Tasks.task_2_1_31(a, b, c)}");
+
+
+
+                //3.2.12
+                List<int> numbers = new List<int> { 2, 5, 11, 15, 25, 4, 35, 36 };
+                Console.Write("Task 3.2.12 Result : { ");
+                foreach (int i in Tasks.task_3_2_12(numbers))
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.Write("}");
+
+                //3.4.10
+                Console.Write($"\nTask 3.4.10 Result : ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"{Tasks.task_3_4_10(i)} ");
+                }
+
+
+                //4.1.43
+                Console.WriteLine($"\nTask 4.1.43 Result : ");
+                int[] data = new int[20];
+                Random rd = new Random();
+                Console.Write("In : ");
+                for (int i = 0; i < 20; i++)
+                {
+                    data[i] = rd.Next(-50, 50);
+                    Console.Write($"{data[i]} ");
+
+                }
+                Console.Write("\nOut : ");
+                foreach (int i in Tasks.task_4_1_43(data))
+                {
+                    Console.Write($"{i} ");
+                }
+
+
+                //4.2.20
+                Console.WriteLine($"\nTask 4.2.20 Result : ");
+                int[,] myData = new int[4, 7];
+                Console.Write("In : \n");
+                for (int i = 0; i < myData.GetLength(0); i++)
+                {
+                    for (int j = 0; j < myData.GetLength(1); j++)
+                    {
+                        myData[i, j] = rd.Next(-100, 100);
+                        Console.Write($"{myData[i, j]}\t");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.Write("\nOut : ");
+                foreach (int num in Tasks.task_4_2_20(myData))
+                {
+                    Console.Write($"{num} ");
+                }
+
+                //4.2.81
+                Console.WriteLine($"\nTask 4.2.81 Result : {Tasks.task_4_2_81(myData)}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
