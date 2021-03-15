e385fb3525c937fae4827d6067e36378f41c8c7c
somebodyys <tkokikaca1@gmail.com>
2021-03-15 21:48:27 +0400

    CLassWork2

---
+++ C#/ClassWork3Solution/ClassWork3/GeoChild.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork3
+{
+    class GeoChild : GeometricFigure
+    {
+
+        private int a;
+        private int b;
+        private int c;
+        private int d;
+        public GeoChild(int sideOne)
+        {
+
+
+        }
+
+        public override float Perimetri()
+        {
+            
+        }
+    }
+}
---
+++ C#/ClassWork3Solution/ClassWork3/GeometricFigure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork3
+{
+    class GeometricFigure
+    {
+
+        public virtual int Perimetri()
+        {
+            return 0;
+        }
+    }
+}
