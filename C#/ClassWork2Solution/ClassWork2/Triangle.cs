6ebf4c9c302ab9b035dfe8cf92501a47b535b060
somebodyys <tkokikaca1@gmail.com>
2021-03-15 21:34:35 +0400

    Finished Work 2

---
+++ C#/ClassWork2Solution/ClassWork2/Triangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork2
+{
+    class Triangle
+    {
+        public static float calcTriangle(int height, int side)
+            => height * side / 2;
+
+        public static float calcTriangle(int sideOne, int sideTwo, int sideThree)
+            => sideOne + sideTwo + sideThree;
+    }
+}
---
+++ C#/ClassWork2Solution/ClassWork2/square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork2
+{
+    class Square
+    {
+        private int side;
+        public int Perimeter = 0;
+
+        public Square(int side)
+        {
+            this.side = side;
+        }
+
+
+        public Square():this(0)
+        {
+            
+        }
+
+        public Square(Square square):this(square.side)
+        {
+            Perimeter = calcPerimeter(square.side);
+        }
+
+        int calcPerimeter(int side)
+            => side * 4;
+
+    }
+}
