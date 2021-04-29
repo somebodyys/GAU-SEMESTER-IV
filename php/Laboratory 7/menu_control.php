97a434e3cdc57b30be656f47e213839b1c777fcb
somebodyys <tkokikaca1@gmail.com>
2021-04-29 14:03:08 +0400

    Finished Half of laboratory 7 Tasks

---
+++ php/Laboratory 7/menu_control.php
@@ -0,0 +1,61 @@
+<?php require_once "db_connect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+        $error = "";
+
+        if(isset($_GET['action']) && $_GET['action'] == "upload" && isset($_POST['add'])){
+            $title = $_POST['Title'];
+            $meta_k = $_POST['Meta_k'];
+            $meta_d = $_POST['Meta_d'];
+            $text = $_POST["Text"];
+            
+            if(strlen($title) > 50) $error.= "Max Title Length 50sym, ";
+            if(strlen($meta_k) > 200 && strlen($meta_d) > 200) $error.= "Max Meta key/Desc length 200sym, ";
+
+            if(empty($error)){
+                $insert_query = "
+                    INSERT INTO menu(Title, Meta_k, Meta_d, Text)
+                    VALUES ('$title', '$meta_k', '$meta_d', '$text')
+                ";
+                if(mysqli_query($conn, $insert_query)) header("Location: menu_control.php");
+                $error = mysqli_error($conn);
+                
+            }
+            
+
+        }
+
+    
+    
+    ?>
+    <h1>Welcone To Menu Editor!</h1>
+
+    <form action="?action=upload" method="post" id="menuEditor">
+        <div id="inputs">
+            <input type="text" placeholder="Title" name="Title" required>
+            <input type="text" placeholder="Meta Key" name="Meta_k" required>
+            <input type="text" placeholder="Meta Describtion" name="Meta_d" required>
+        </div>
+        
+        <textarea name="Text" id="" cols="30" rows="10"></textarea>
+        <button name="add">Add</button>
+    </form>
+
+    <div id="result">
+        <div id="error"><?=$error?></div>
+    </div>
+</body>
+
+</html>
---
+++ php/Laboratory 7/user_control.php
@@ -0,0 +1,59 @@
+<?php require_once "db_connect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+        $error = "";
+        $reg_date = date("Y-m-d");
+        if(isset($_GET['action']) && $_GET['action'] == "addUser" && isset($_POST['Name'])){
+            $fname = $_POST['Name'];
+            $lname = $_POST['Lastname'];
+            $age = $_POST['Age'];
+            $date = $_POST['Date'];
+            $password = $_POST['Password'];
+            $gender = $_POST['Gender'];
+            $user_query = "
+                INSERT INTO users(Name, Lastname, Age, Date, Red_Date, Password, Gender)
+                VALUES ('$fname', '$lname', '$age', '$date', '$reg_date', '$password', '$gender')
+            ";
+            if(mysqli_query($conn, $user_query)) header("Location: user_control.php");
+            $error = mysqli_error($conn);
+
+        }
+    
+    ?>
+
+    <h1>User Control!</h1>
+    <form action="?action=addUser" method="post" id="userEditor">
+        <input type="text" name="Name" placeholder="First Name" required>
+        <input type="text" name="Lastname" placeholder="Last Name" required>
+        <input type="number" name="Age" placeholder="Age" required>
+        <input type="date" name="Date" required>
+        <input type="password" name="Password" placeholder="PassWord" required>
+
+
+        <div id="sub">
+            <select name="Gender" required>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </select>
+            <button>Add User</button>
+        </div>
+        
+    </form>
+
+    <div id="result">
+        <div id="error"><?=$error?></div>
+    </div>
+</body>
+
+</html>
